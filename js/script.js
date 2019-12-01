7e26198fe2972652320d33f1e0072deac58a5222
roshanbandara <47655552+roshanbandara@users.noreply.github.com>
2019-12-01 11:36:37 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
           offset: '100px'
      });
 
-
 // Scroll on main cta buttons ==============================================================================================================
 /*     $('.js--scroll-to-plans').click(function() {
           $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
